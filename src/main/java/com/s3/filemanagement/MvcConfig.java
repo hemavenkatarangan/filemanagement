56d8c42edaf6102c5bc075a184b84eccdecee1ac
hema.venkatarangan <hema.venkatarangan@L46JZYD3.temenosgroup.com>
2022-04-29 10:42:23 +0530

    status

--- src/main/java/com/s3/filemanagement/MvcConfig.java
+++ src/main/java/com/s3/filemanagement/MvcConfig.java
@@ -4,10 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class MvcConfig implements WebMvcConfigurer {
+public class MvcConfig extends WebMvcConfigurerAdapter  {
     
     
     private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
@@ -21,18 +22,16 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
     
-    
-        if (!registry.hasMappingForPattern("/webjars/**")) {
-    
-    
-            registry.addResourceHandler("/webjars/**").addResourceLocations(
-                    "classpath:/META-INF/resources/webjars/");
-        }
+    	registry.addResourceHandler("/**").addResourceLocations(
+                "file:src/main/resources/public/"
+            );
+        
         if (!registry.hasMappingForPattern("/**")) {
     
     
             registry.addResourceHandler("/**").addResourceLocations(
                     CLASSPATH_RESOURCE_LOCATIONS);
+            
         }   
     }
  }
